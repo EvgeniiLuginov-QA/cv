86770a59318a7e01294b8071a3df028a42c11bbc
mm.ee.108@gmail.com <mm.ee.108@gmail.com>
2022-09-22 23:13:35 +0700

    credo_test

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert("В чем сложность? \u{1f383}")
+	alert("«Тестировщик должен знать продукт лучше всех» \u{1f383}")
 })
